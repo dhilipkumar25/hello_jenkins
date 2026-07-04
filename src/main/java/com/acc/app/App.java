dd69d9d22793a0d9903dca7eebfcda3775b56874
dhilipkumar25 <dhilipkumarddhandapani@gmail.com>
2026-07-04 21:41:59 +0530

    test chaining

--- src/main/java/com/acc/app/App.java
+++ src/main/java/com/acc/app/App.java
@@ -5,6 +5,6 @@ package com.acc.app;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! Test chaining");
     }
 }
